2dd9b96af305e5c12f4f4900afd8d2d91f7b94a9
Jason <jdclaeys@outlook.com>
2019-11-02 16:18:24 -0500

    removed a line from index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,15 @@
-// Accordion Init
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems);
-  });
-
 // Dropdown Menu Init
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems);
   });
 
-// Current time and date
-var now = moment();
+
+// Accordion Init
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+  });
 
 // Get current year for copyright
 document.getElementById('year').textContent = new Date().getFullYear();
